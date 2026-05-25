28b604ef2821ead9354b386f570a9dd53410d47d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-05-25 16:59:45 -0400

    Add `/about` endpoint with HTMLResponse and typing annotations

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get('/about', response_class=HTMLResponse)
+def read_about() -> str:
+    return 'An exceptional company'
